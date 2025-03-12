2788f40b7a19c80cea14f1e56054785dcdb194b6
Nekeisha Ashlyn Elfa <71298136+neelfa@users.noreply.github.com>
2025-03-13 03:04:24 +0800

    Update payment.blade.php

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -96,7 +96,7 @@ $subscriptionId = request()->id; // Get subscription ID from URL
     }
 
     .amount-box {
-        background: #F0F4FF;
+        background: #FFFFFF;
         padding: 15px;
         border-radius: 8px;
         margin: 10px 0;
